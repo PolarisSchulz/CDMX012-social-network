2caea4beb1fe267e19b9d4023214a290b42acc06
IvBrt <crstn.b7@gmail.com>
2022-04-15 17:30:23 -0500

    vista registro de usuario con todos sus elementos

--- src/components/Registro.js
+++ src/components/Registro.js
@@ -10,10 +10,36 @@ export const Registro = () => {
 
   const nodoH2 = document.createElement("h2");
   nodoH2.textContent = "Registro de usuario";
+  
+  //aquí estan los inputs para el registro
+  const datosUsuarioDiv = document.createElement("form");
+  datosUsuarioDiv.className = "datosUsuarioRegistro";
+  //input para nombre
+  const nombreRegistroInput = document.createElement("input");
+  nombreRegistroInput.setAttribute("id", "nombreRegistroInput");
+  nombreRegistroInput.setAttribute("placeholder", "Nombre");
+  // input para correo
+  const correoRegistroInput = document.createElement("input");
+  correoRegistroInput.setAttribute("id", "correoRegistroInput");
+  correoRegistroInput.setAttribute("placeholder", "Correo");
+  // input para contraseña
+  const contrasenaRegistroInput = document.createElement("input");
+  contrasenaRegistroInput.setAttribute("id", "contrasenaRegistroInput");
+  contrasenaRegistroInput.setAttribute("placeholder", "Contraseña");
+  //input confirmación de contraseña
+  const confirmarContrasenaInput = document.createElement("input");
+  confirmarContrasenaInput.setAttribute("id", "confirmarContrasenaInput");
+  confirmarContrasenaInput.setAttribute("placeholder", "Confirmar contraseña");
+  //boton de registro
+  const registroInput = document.createElement("input");
+  registroInput.setAttribute("type", "submit");
+  registroInput.setAttribute("id", "registroInput");
+  registroInput.setAttribute("value", "Registrarme");
+
+
 
   const regresarDiv = document.createElement("div");
   const botonRegresarVistaRegistro = document.createElement("button");
-
   botonRegresarVistaRegistro.textContent = "Regresar";
   botonRegresarVistaRegistro.addEventListener("click", () => {
     navegador("/");
@@ -23,6 +49,12 @@ export const Registro = () => {
     logoVistaRegistroDiv,
     logoVistaRegistroImagen,
     nodoH2,
+    datosUsuarioDiv,
+    nombreRegistroInput,
+    correoRegistroInput,
+    contrasenaRegistroInput,
+    confirmarContrasenaInput,
+    registroInput,
     regresarDiv,
     botonRegresarVistaRegistro
   );
